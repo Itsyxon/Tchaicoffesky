38abc3ef07336fd046192a4ec80ce6319890cf17
ditsyxon <ditsyxon@excitekit.ru>
2024-11-19 02:48:55 +0300

    added images domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    formats: ['image/webp'],
+    domains: ['tchaicoffesky.netlify.app'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
